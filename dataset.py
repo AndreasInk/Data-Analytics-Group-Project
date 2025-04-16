50a27c2c3e739ad3b21ac6889b281876fd258946
SteveDaSteve <steven@addressplus.net>
2025-04-16 14:09:58 -0400

    Implemented MinMax normalization

--- dataset.py
+++ dataset.py
@@ -1,7 +1,7 @@
 import kagglehub 
 import pandas as pd
 import os
-from sklearn import preprocessing
+from sklearn import preprocessing as pre
 
 def load_voice_dataset() -> pd.DataFrame:
     """
@@ -29,8 +29,20 @@ def preprocess_dataset(df) -> pd.DataFrame:
 
 # Take every numeric value in the dataset and normalize it to a value between 0 and 1
 def normalize_dataset(df) -> pd.DataFrame:
+    # Drop the columns that dont need to be normalized. static_attributes stores the names of the attributes left out of normalization (strings and binary values)
+    static_attributes = ["Unnamed: 0", "name", "status"]
+    df_normalizable = df.drop(static_attributes, axis=1)
+    
+    # Prep the scaler. MinMaxScaler normalizes all the values to between 0 and 1, using the max and min from each attribute
+    min_max_scaler = pre.MinMaxScaler((0, 1))
+    # Apply the normalization
+    df_normalized = min_max_scaler.fit_transform(df_normalizable)
 
-    return df
+    # Pack the returned numpy array into a DataFrame, making sure the original labels and indexing carry over
+    df_normalized = pd.DataFrame(df_normalized, columns=df_normalizable.columns, index=df_normalizable.index)
+    # Merge the normalized dataset with the attributes left out of normalization. This does change the ordering of the attributes, with the ones left out appearing at the front.
+    df_normalized = pd.concat([df[static_attributes], df_normalized], axis=1)
+    return df_normalized
 
 # Remove the outliers from the dataset
 def remove_outliers(df) -> pd.DataFrame:
